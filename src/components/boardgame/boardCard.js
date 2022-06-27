8304bc14655031219e3c1ecd4e4169632b58d2c6
ostanmadalina92 <ostan.madalina@yahoo.com>
2022-06-27 16:22:43 +0300

    boardgame component - structure and styling

---
+++ src/components/boardgame/boardCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Xicon from "../icons/Xicon";
+import Oicon from "../icons/Oicon";
+
+const BoardCard = ({ user = "nouser", active, index }) => {
+  return (
+    <div
+      className={`card ${ active && user === "x" && "shadow-blue"} 
+      ${active && user === "o" && "shadow-yellow"} ${!active ? "shadow-gray" : "active"}`}
+    >
+      {user === "x" && <Xicon color={active && "dark"} size="lg" />}
+      {user === "o" && <Oicon color={active && "dark"} size="lg" />}
+    </div>
+  );
+};
+
+export default BoardCard;
