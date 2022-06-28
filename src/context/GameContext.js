2612a8a5160c6cb06d2de3cadc3dca9fc2cc1335
ostanmadalina92 <ostan.madalina@yahoo.com>
2022-06-28 17:33:29 +0300

    calculate winner

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,38 +1,56 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
+import { calcWinner } from "../utilities/calcSquares";
+import { WinStateContext } from "./winStateContext";
 
 const GameContext = createContext();
 
 const GameState = (props) => {
+  const { showWinState, setWinStateMode } = useContext(WinStateContext);
+
   const [screen, setScreen] = useState("start");
 
   const [activeUser, setActiveUser] = useState("x");
   const [playMode, setPlayMode] = useState("user");
 
-  const [squares, setSquares] = useState(new Array(9).fill(''));
+  const [squares, setSquares] = useState(new Array(9).fill(""));
   const [xnext, setXnext] = useState(false);
 
-  const changePlayMode =  mode => {
-      setPlayMode(mode);
-      setScreen("game");
-  }
+  const [winner, setWinner] = useState(null);
+  const [winnerLine, setWinnerLine] = useState(null);
+
+  const [ties, setTies] = useState({x: 0 , o:0})
+
+  const changePlayMode = (mode) => {
+    setPlayMode(mode);
+    setScreen("game");
+  };
 
   const handleSquareClick = (ix) => {
-    
-    const currentUser = xnext ? 'o' : 'x';
-    if(playMode === 'cpu' && currentUser !== activeUser){
-        return;
+    const currentUser = xnext ? "o" : "x";
+    if (playMode === "cpu" && currentUser !== activeUser) {
+      return;
     }
 
     let ns = [...squares];
-    ns[ix] = !xnext ? 'x' : 'o';
+    ns[ix] = !xnext ? "x" : "o";
 
     setSquares(ns);
     setXnext(!xnext);
+    checkWinner(ns);
+  };
+  const checkWinner = (ns) => {
+    const isWinner = calcWinner(ns);
+    if (isWinner) {
+      setWinner(isWinner.winner);
+      setWinnerLine(isWinner.line);
 
-  }
-    const checkWinner = (ns) => {
-        
+      const ti = {...ties}
+      ti[isWinner.winner] += 1 
+      setTies(ti)
+      showWinState();
+      setWinStateMode("winner");
     }
+  };
   return (
     <GameContext.Provider
       value={{
